f80a39eaa56bf4c4542a4258646a5e7819a2783d
ikeman32 <ikeman32@yaho.com>
2019-10-04 08:35:03 -0700

    Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,5 +10,25 @@
 // And add it to the DOM in the .header-container component
 
 function Header() {
+    //create elements
+    const
+        newHeader = document.createElement('div'),
+        newDate = document.createElement('span'),
+        newH1 = document.createElement('h1'),
+        newTemp = document.createElement('span');
 
+    //Structuring
+    newHeader.appendChild(newDate);
+    newHeader.appendChild(newH1);
+    newHeader.appendChild(newTemp);
+
+    //Create classes
+
+    newHeader.classList.add('header');
+    newDate.classList.add('date');
+    newTemp.classList.add('temp');
+
+    return newHeader;
 }
+
+document.querySelector('.header-container').appendChild(Header());
