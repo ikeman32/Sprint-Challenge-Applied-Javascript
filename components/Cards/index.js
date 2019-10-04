22e0823de896603271e7fe415c073e9c27079769
ikeman32 <ikeman32@yaho.com>
2019-10-04 11:01:02 -0700

    Cards not done

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,64 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get(' https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => {
+        //console.log(res.data.articles.javascript);
+        //res.data.articles.javascript;
+        // for (let i = 0; i < theTopics.length; i++) {
+        //     console.log(theTopics[i]);
+        // }
+        for (let [key, value] of Object.entries(res.data.articles)) {
+            console.log(`${key}: ${value}`);
+        }
+
+
+    })
+    .catch(error => {
+        alert(`So terribly sorry but there was an ${error} articles`);
+    });
+let theTopics = [];
+const topics = axios.get(' https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        for (let [key, value] of Object.entries(response.data.topics)) {
+
+        }
+
+    })
+    .catch(error => {
+        alert(`So terribly sorry but there was an ${error} topics`);
+    });
+
+
+
+function Articles(headline, img, author) {
+    //create elements
+    const
+        newCard = document.createElement('div'),
+        newHeadline = document.createElement('div'),
+        newAuthor = document.createElement('div'),
+        newContainer = document.createElement('div'),
+        newImg = document.createElement('img'),
+        newBy = document.createElement();
+
+    //Structure
+    newCard.appendChild(newHeadline);
+    newCard.appendChild(newAuthor);
+    newAuthor.appendChild(newContainer);
+    newAuthor.appendChild(newBy);
+    newContainer.appendChild(newImg);
+
+    //Classes
+    newCard.classList.add('card');
+    newHeadline.classList.add('headline');
+    newAuthor.classList.add('author');
+    newContainer.classList.add('img-container');
+
+    //content
+    newHeadline.textContent = headline;
+    newImg.src = img;
+    newBy.textContent = `By ${author}`;
+
+    return newCard
+
+}
